897096d34073a44a4d4c75861fd9b6e6c5377cdd
vanbui93 <van.bt.38@gmail.com>
2020-08-09 18:15:26 +0700

    thêm Authorization vao header cua netword

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import setAuthorizationToken from './../utils/setAuthorizationToken';
 
 export function userLoginRequest (userData) {
     return dispatch => {
         return axios.get('http://localhost:3000/users', userData).then(res => {
-            const token = res.data;
-            localStorage.setItem('jwtToken', JSON.stringify(token));
+            const token = JSON.stringify(res.data);
+            localStorage.setItem('jwtToken', token);
+            setAuthorizationToken(token)
         });
     }
 }
---
+++ src/utils/setAuthorizationToken.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+//thêm Authorization vào trong header của netword
+export default function setAuthorizationToken(token) {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+}
